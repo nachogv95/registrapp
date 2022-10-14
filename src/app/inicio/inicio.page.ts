924f02639c2e30ae4b0a048e7e80a72ab7c7b44e
Nacho <nachogonzavid95@gmail.com>
2022-10-14 00:05:55 -0300

    update

--- src/app/inicio/inicio.page.ts
+++ src/app/inicio/inicio.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-inicio',
@@ -6,8 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inicio.page.scss'],
 })
 export class InicioPage implements OnInit {
-
-  constructor() { }
+  user: any;
+  constructor(private activeroute: ActivatedRoute, private router: Router, public alertController: AlertController) {
+    // Se llama a la ruta activa y se obtiene sus parametros mediante una subscripcion
+    this.activeroute.queryParams.subscribe(params => { // Utilizamos lambda
+      if (this.router.getCurrentNavigation().extras.state) { // Validamos que en la navegacion actual tenga extras
+        this.user = this.router.getCurrentNavigation().extras.state.user; // Si tiene extra rescata lo enviado
+        console.log(this.user) // Muestra por consola lo traido
+      }else{this.router.navigate(["/login"])} // Si no tiene extra la navegacion actual navegar al login
+    });
+  }
 
   ngOnInit() {
   }
